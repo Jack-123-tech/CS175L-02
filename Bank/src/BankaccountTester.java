d5e85cab4e6ec6c87eeb20a7b1235bf45045fc84
megam <megam@LAPTOP-PMEEBIU5>
2021-03-03 19:17:40 -0500

    This is the thrid edit for Bank with mod

--- Bank/src/BankaccountTester.java
+++ Bank/src/BankaccountTester.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	Scanner in = new Scanner(System.in);
 	System.out.println("Enter amount to start the account:");
 	double startBal= in.nextDouble();
-	System.out.println("Enter Interest rat for this account:");
+	System.out.println("Enter Interest rate for this account:");
 	double InterstPct= in.nextDouble();
 	
 	
@@ -16,24 +16,24 @@ public static void main(String[] args) {
  	
  	 
  	 
- 	System.out.println("Created new account with:" + startBal); System.out.println("and intrest rate of"  +InterstPct);
+ 	System.out.println("\t Created new account with:" + startBal + " 1and intrest rate of:"  +InterstPct);
  	
  	
- 	System.out.println("Enter how much you would like to deposit:");
+ 	System.out.println(" Enter how much you would like to deposit:");
  	double depositamount= in.nextDouble();
  	
  	myBankAccount.deposit(depositamount);
  	
- 	System.out.println("Enter how much you would like to withdraw:");
+ 	System.out.println("\t Enter how much you would like to withdraw:");
  	double withdrawamount= in.nextDouble();
  	in.nextLine();
  	myBankAccount.withdraw(withdrawamount);
  	
- 	System.out.println("Enter how much you would like to withdraw:");
+ 	System.out.println("\t Enter how much you would like to withdraw:");
  	double withdrawamount2=in.nextDouble();
  	in.nextLine();
  	myBankAccount.withdraw(withdrawamount2);
- 	System.out.println("Do you want to generate your interest, enter 'Yes'");
+ 	System.out.println("\t Do you want to generate your interest, enter 'Yes'");
  	String response=in.nextLine();
  	
  	if (response.equals("Yes") ) {
